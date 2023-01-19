4671944241fe9c74d2e8dfdd942c9bdba34e9d0b
Bryan <9421742+bmmiller24@users.noreply.github.com>
2023-01-19 16:40:23 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import ListCast from './components/ListCast';
 import Modals from './components/Modals';
+import Nav from './components/Nav';
 
 function App() {
-  const name = 'StarGazers'
   let [memberInfo, setMemberInfo] = useState(null);
   const [cast, setCast] = useState([]);
 
@@ -17,17 +17,20 @@ function App() {
   });
 
   return (
-    <div className="container">
-      <hgroup>
-        <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>Meet the Stargazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo &&
-          <Modals member={memberInfo} handleClose={() => setMemberInfo(null)} />
-        }
-      </hgroup>
-    </div>
+    <>
+      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+      <div className="container">
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the Stargazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+          {memberInfo &&
+            <Modals member={memberInfo} handleClose={() => setMemberInfo(null)} />
+          }
+        </hgroup>
+      </div>
+    </>
   )
 }
 export default App
